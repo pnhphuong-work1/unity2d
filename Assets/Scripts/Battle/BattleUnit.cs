51e05d52ec5bf4d14c7beee6c8d06703d26eb1d2
lqviet45 <lqviet455@gmail.com>
2024-06-12 12:26:20 +0700

    adding dotween animation source

--- Assets/Scripts/Battle/BattleUnit.cs
+++ Assets/Scripts/Battle/BattleUnit.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,12 +12,24 @@ public class BattleUnit : MonoBehaviour
 
     public Character Char { get; set; } 
     
+    private Image _image;
+
+    private void Awake()
+    {
+        _image = GetComponent<Image>();
+    }
+
     public void SetUp()
     {
         Char = new Character(_base, level);
         if (isPlayerUnit)
-            GetComponent<Image>().sprite = Char.Base.FrontSprite;
+            _image.sprite = Char.Base.FrontSprite;
         else
-            GetComponent<Image>().sprite = Char.Base.FrontSprite;
+            _image.sprite = Char.Base.FrontSprite;
+    }
+    
+    public void PlayBattleEnterAnimation()
+    {
+        
     }
 }
