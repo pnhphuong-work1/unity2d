f089cba3c624956f294d4cacdb788e5865bbd741
Phan Nguyễn Hoàng Phương <pnhphuong.work1@gmail.com>
2024-06-13 01:37:55 +0700

    asset

---
+++ Assets/Scripts/Action/MainMenuAction.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuAction : MonoBehaviour
+{
+    public void PlayButton_OnClick()
+    {
+    }
+    public void ExitButton_OnClick()
+    {
+        
+    }
+    
+}
